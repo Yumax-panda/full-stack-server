ba5bffac3bf731607f4f17a7b4f5bbe0cc17e76d
Yumax-panda <yuma0wall@gmail.com>
2023-12-04 18:37:07 +0900

    ログイン、新規登録のDB操作を追加

--- model/user.go
+++ model/user.go
@@ -4,19 +4,54 @@ import (
 	"database/sql"
 
 	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
 	_ "gorm.io/gorm"
 )
 
 type User struct {
-	ID       uuid.UUID
-	Name     string
-	Email    string
-	Password string
-	Comment  sql.NullString
+	ID       uuid.UUID      `json:"id"`
+	Name     string         `json:"name"`
+	Email    string         `json:"email"`
+	Password string         `json:"password"`
+	Comment  sql.NullString `json:"comment"`
 }
 
 func GetUser() ([]User, error) {
 	var users []User
 	err := db.Find(&users).Error
 	return users, err
+}
+
+func CreateUser(name string, email string, password string, comment string) (User, error) {
+	user := User{
+		ID:       uuid.New(),
+		Name:     name,
+		Email:    email,
+		Password: password,
+		Comment:  sql.NullString{String: comment, Valid: true},
+	}
+	err := db.Create(&user).Error
+	return user, err
+}
+
+func GetUserByEmail(email string) (User, error) {
+	var user User
+	err := db.Where("email = ?", email).First(&user).Error
+	return user, err
+}
+
+func comparePassword(hashedPassword string, password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+	return err == nil
+}
+
+func Login(email string, password string) (User, error) {
+	user, err := GetUserByEmail(email)
+	if err != nil {
+		return user, err
+	}
+	if !comparePassword(user.Password, password) {
+		return user, err
+	}
+	return user, nil
 }
